bc458f04f323730266a865be228182926991c912
shafna <shafnav13@gmail.com>
2025-06-24 15:20:57 +0530

    back

---
+++ backend/routes/items.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const router = express.Router();
+const Item = require('../models/Item');
+
+// GET all items
+router.get('/', async (req, res) => {
+  const items = await Item.find();
+  res.json(items);
+});
+
+// POST a new item
+router.post('/', async (req, res) => {
+  const newItem = new Item(req.body);
+  const savedItem = await newItem.save();
+  res.status(201).json(savedItem);
+});
+
+// PUT update item
+router.put('/:id', async (req, res) => {
+  const updated = await Item.findByIdAndUpdate(req.params.id, req.body, { new: true });
+  res.json(updated);
+});
+
+// DELETE item
+router.delete('/:id', async (req, res) => {
+  await Item.findByIdAndDelete(req.params.id);
+  res.json({ message: 'Item deleted' });
+});
+
+module.exports = router;
